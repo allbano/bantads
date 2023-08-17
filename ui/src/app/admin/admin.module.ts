a269d3f3b81b17511341645c4858c965903565a3
fvicente95 <74841976+fvicente95@users.noreply.github.com>
2023-08-17 11:39:13 -0300

    Add files via upload
    Modulo e componente administrador

---
+++ ui/src/app/admin/admin.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TelaInicialAdminComponent } from './tela-inicial-admin/tela-inicial-admin.component';
+
+
+
+@NgModule({
+  declarations: [
+    
+  
+    TelaInicialAdminComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
---
+++ ui/src/app/admin/tela-inicial-admin/tela-inicial-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-inicial-admin',
+  templateUrl: './tela-inicial-admin.component.html',
+  styleUrls: ['./tela-inicial-admin.component.scss']
+})
+export class TelaInicialAdminComponent {
+
+}
