94551b93e6d97b7469ec97f6aee9fe50570c5b0b
Lucas Ribeiro Carvalho <ribeirolucas149@gmail.com>
2023-08-17 00:14:04 -0300

    módulo gerente criado

---
+++ ui/src/app/gerente/gerente.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TelaInicialGerenteComponent } from './tela-inicial-gerente/tela-inicial-gerente.component';
+
+
+
+@NgModule({
+  declarations: [
+    TelaInicialGerenteComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GerenteModule { }
