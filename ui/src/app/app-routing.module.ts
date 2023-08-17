1d3520922950cefd79f5d1d68bc7210f34dbc18b
Lucas Ribeiro Carvalho <ribeirolucas149@gmail.com>
2023-08-17 00:16:31 -0300

    configuração de rota tela-inicial-gerente

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TelaInicialGerenteComponent } from './gerente/tela-inicial-gerente/tela-inicial-gerente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'gerente', component: TelaInicialGerenteComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
