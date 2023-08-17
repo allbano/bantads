5d7e9c5876cca47115b72fd603e5c503dd277861
Lucas Ribeiro Carvalho <ribeirolucas149@gmail.com>
2023-08-17 00:14:54 -0300

    componente tela-inicial-gerente criado

---
+++ ui/src/app/gerente/tela-inicial-gerente/tela-inicial-gerente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-inicial-gerente',
+  templateUrl: './tela-inicial-gerente.component.html',
+  styleUrls: ['./tela-inicial-gerente.component.scss']
+})
+export class TelaInicialGerenteComponent {
+
+}
