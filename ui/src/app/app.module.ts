e1a1cfd0830c9bb1621359ba26cd162d5ce6678a
Albano Maywitz <allbano@gmail.com>
2023-08-20 02:54:36 -0300

    Reorganizado o projeto parte 1: Título e MatToolbar.

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GerenteModule } from './gerente/gerente.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     declarations: [
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         AppRoutingModule,
         GerenteModule,
         BrowserAnimationsModule,
+        MatToolbarModule
     ],
     providers: [],
     bootstrap: [AppComponent]
