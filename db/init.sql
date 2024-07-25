140d78e7c2d3baf33c527a6365dac6f4c9d4fccb
Albano Maywitz <allbano@gmail.com>
2024-07-25 00:22:25 -0300

    Adicionado os arquivos de configuração do postgresql para o trabalho.

---
+++ db/init.sql
@@ -0,0 +1,25 @@
+-- Script para criar um slot de replicação física
+DO
+$$
+BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_replication_slots
+        WHERE slot_name = 'slot_replication_master'
+    ) THEN
+        PERFORM pg_create_physical_replication_slot('slot_replication_master');
+    END IF;
+END
+$$;
+
+-- Criar o papel userbackup com permissões de replicação
+DO
+$$
+BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_roles
+        WHERE rolname = 'userbackup'
+    ) THEN
+        CREATE ROLE userbackup WITH LOGIN ENCRYPTED PASSWORD 'testing' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT REPLICATION CONNECTION LIMIT -1;
+    END IF;
+END
+$$;
